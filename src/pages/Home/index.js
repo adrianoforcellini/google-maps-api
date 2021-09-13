f268b9c30cef50431488afc9d3ed7ab1f2753923
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-13 19:34:02 -0300

    Home page + css to 'provider' basico

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Search } from './styles';
+
+const Home = () => (
+    <Container>
+        <Search/>
+        <h1>xablau</h1>
+    </Container>
+)
+export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background-color: ${props => props.theme.colors.primary};
+width: 360px;
+height:100vH;
+overflox-y: scroll;
+
+`;
+
+export const Search = styled.section`
+dysplay: flex;
+flex-direction: column;
+justify-content: center;
+background color: #ffffff;
+`;
