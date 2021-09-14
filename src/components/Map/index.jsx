49650784f6391e2b72aba8c1c12fb9f81c639b43
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-14 18:07:06 -0300

    iniciando mapa

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
+
+export const MapContainer = (props) => {};
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBUYC0-Ep-52DVi9mSbQCJRrJJb9Cy6wGs",
+  language: "pt-BR",
+})(MapContainer)
