2d4c7e3aa4907ed10e4b9d91924418ce29a8f109
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-13 22:16:42 -0300

    carrousel ->  sem necessidade de tag img

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -2,23 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
-  width: 90%;
-  height: 90%;
+  width: 40vw;
+  height: 40vh;
   border-radius: 6px;
-  background-image: ${(props) => props.photo};
+  background-image: url(${(props) => props.photo}) ;
   background-size: cover;
 `;
 
-const Title = styled.div`
+const Title = styled.p`
+  margin-top: 10%;
   font-family: ${(props) => props.theme.fonts.regular};
   color: white;
-  font-size: 16px;
+  font-size: 18px;
 `;
 
 const ImgCard = ({ photo }) => (
   <Card photo={photo}>
-    <Title>Nome</Title>
-    <img alt="imagens dos restaurantes" src={photo}/>
+    <Title>Nome Do Restaurante</Title>
   </Card>
 );
 
